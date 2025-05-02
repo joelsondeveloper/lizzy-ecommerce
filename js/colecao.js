4fd6c82d2c3229c6bd376adf2ac1037d4c697f94
JoelsonDeveloper <vicentejoelson80@gmail.com>
2025-05-02 08:53:05 -0300

    novo carrosel

--- js/colecao.js
+++ js/colecao.js
@@ -1,24 +1,38 @@
 const colecao = document.querySelector(".cards-colecoes");
+const sugestãos = document.querySelector(".sugestoes-container");
 const dimensoesColecao = colecao.getBoundingClientRect();
-const prevBtnColecao = document.getElementById("prevBtnColecao");
-const nextBtnColecao = document.getElementById("nextBtnColecao");
+const dimensoesSugestoes = sugestãos.getBoundingClientRect();
+const prevBtnColecao = Array.from(document.querySelectorAll(".prevBtn"));
+const nextBtnColecao = Array.from(document.querySelectorAll(".nextBtn"));
 
 function verificarScroll() {
     console.log(colecao.scrollLeft);
     margenErro = 250;
     const maxScroll = colecao.scrollWidth - colecao.clientWidth;
-    prevBtnColecao.style.display = colecao.scrollLeft > margenErro ? "block" : "none";
-    nextBtnColecao.style.display = colecao.scrollLeft < ( maxScroll - margenErro) ? "block" : "none";
+    prevBtn.style.display = colecao.scrollLeft > margenErro ? "block" : "none";
+    nextBtn.style.display = colecao.scrollLeft < ( maxScroll - margenErro) ? "block" : "none";
 }
 
-nextBtnColecao.addEventListener("click", () => {
-    colecao.scrollLeft += dimensoesColecao.width;
-    setTimeout(verificarScroll, 300);
-});
+nextBtnColecao.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (btn == nextBtnColecao[0]) {
+            colecao.scrollLeft += dimensoesColecao.width;
+        } else if (btn == nextBtnColecao[1]) {
+            sugestãos.scrollLeft += dimensoesSugestoes.width;
+        }
+        setTimeout(verificarScroll, 300);
+    });
+})
 
-prevBtnColecao.addEventListener("click", () => {
-    colecao.scrollLeft -= dimensoesColecao.width;
-    setTimeout(verificarScroll, 300);
-});
+prevBtnColecao.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (btn == prevBtnColecao[0]) {
+            colecao.scrollLeft -= dimensoesColecao.width;
+        } else if (btn == prevBtnColecao[1]) {
+            sugestãos.scrollLeft -= dimensoesSugestoes.width;
+        }
+        setTimeout(verificarScroll, 300);
+    });
+})
 
 verificarScroll();
